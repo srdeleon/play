87ba2d990fee96127d9b86d1daaeead59d1d67a5
srdeleon <silvio.deleon@gmail.com>
2017-04-29 03:58:47 -0400

    Completed Chapter 6

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -1,67 +1,91 @@
 package controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import models.Product;
 import models.Tag;
 import play.data.Form;
-import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.products.list;
-import views.html.products.details;
+import play.mvc.Controller;
+import play.mvc.Http.MultipartFormData;
+import play.mvc.Http.MultipartFormData.FilePart;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-@Catch
+import com.google.common.io.Files;
+
+import views.html.products.*;
+
+
 public class Products extends Controller {
-	
-	private static final Form<Product> productForm = Form.form(Product.class);
-	
-	public static Result index() {
-		return redirect(routes.Products.list(0));
-	}
-	
-	public static Result list(Integer page) {
-		List<Product> products = Product.findAll();
-		return ok(list.render(products));
-	}
-	
-	public static Result newProduct() {
-		return ok(details.render(productForm));
-	}
-	
-	public static Result details(Product product) {
-		Form<Product> filledForm = productForm.fill(product);
-		return ok(details.render(filledForm));
-	}
-	
-	public static Result save() {
-		Form<Product> boundForm = productForm.bindFromRequest();
-		if(boundForm.hasErrors()) {
-			return ok(details.render(boundForm));
-		}
-		Product product = boundForm.get();
-		
-		List<Tag> tags = new ArrayList<Tag>();
-		for (Tag tag : product.tags) {
-			if (tag.id != null) {
-				tags.add(Tag.findById(tag.id));
-			}
-		}
-		product.tags = tags;
-		product.save();
-		flash("success", String.format("Successfully added product %s", product));
-		
-		return redirect(routes.Products.list(1));
-	}
-	
-	public static Result delete(String ean) {
-		final Product product = Product.findByEan(ean);
-		if (product == null) {
-			return notFound(String.format("Product %s does not exists.", ean));
-		}
-		
-		Product.remove(product);
-		return redirect(routes.Products.list(1));
-	}
 
+  private static final Form<Product> productForm = Form.form(Product.class);
+
+  public static Result index() {
+    return redirect(routes.Products.list(1));
+  }
+
+  public static Result list(Integer page) {
+    List<Product> products = Product.findAll();
+    return ok(list.render(products));
+  }
+
+  public static Result newProduct() {
+    return ok(details.render(productForm));
+  }
+
+  public static Result details(Product product) {
+    Form<Product> filledForm = productForm.fill(product);
+    return ok(details.render(filledForm));
+  }
+
+  public static Result save() {
+    Form<Product> boundForm = productForm.bindFromRequest();
+    if(boundForm.hasErrors()) {
+      flash("error", "Please correct the form below.");
+      return badRequest(details.render(boundForm));
+    }
+
+    Product product = boundForm.get();
+    MultipartFormData body = request().body().asMultipartFormData();
+    FilePart part = body.getFile("picture");
+    
+    if( part != null) {
+        File picture = part.getFile();
+        try {
+            product.picture = Files.toByteArray(picture);
+        } catch (IOException e) {
+            return internalServerError("Error reading file upload");
+        }
+    }
+
+    List<Tag> tags = new ArrayList<Tag>();
+    for (Tag tag : product.tags) {
+      if (tag.id != null) {
+        tags.add(Tag.findById(tag.id));
+      }
+    }
+    product.tags = tags;
+
+    product.save();
+    flash("success",
+        String.format("Successfully added product %s", product));
+
+    return redirect(routes.Products.list(1));
+  }
+
+  public static Result delete(String ean) {
+    final Product product = Product.findByEan(ean);
+    if(product == null) {
+        return notFound(String.format("Product %s does not exists.", ean));
+    }
+    Product.remove(product);
+    return redirect(routes.Products.list(1));
+  }
+  
+  public static Result picture(String ean) {
+      final Product product = Product.findByEan(ean);
+      if(product == null) return notFound();
+      return ok(product.picture);
+  }
 }
--- app/models/Product.java
+++ app/models/Product.java
@@ -1,96 +1,151 @@
 package models;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import play.data.validation.Constraints.Required;
+
+import play.data.validation.Constraints;
+import play.libs.F;
 import play.mvc.PathBindable;
-import utils.EAN;
+import play.mvc.QueryStringBindable;
 
-public class Product implements PathBindable<Product> {
-	
-	@EAN
-	public String ean;
-	
-	@Required
-	public String name;
-	
-	public String description;
-	
-	public List<Tag> tags = new LinkedList<Tag>();
-	
-	public Product() {}
-	
-	public Product (String ean, String name, String description) {
-		this.ean = ean;
-		this.name = name;
-		this.description = description;
-	}
-	
-	public String toString() {
-		return String.format("%s - %s", ean, name);
-	}
-	
-	private static List<Product> products;
-	
-	static {
-		products = new ArrayList<Product>();
-		products.add(new Product("1111111111111", "Paperclipse 1", "Paperclips description 1"));
-		products.add(new Product("2222222222222", "Paperclipse 2", "Paperclips description 2"));
-		products.add(new Product("3333333333333", "Paperclipse 3", "Paperclips description 3"));
-		products.add(new Product("4444444444444", "Paperclipse 4", "Paperclips description 4"));
-		products.add(new Product("5555555555555", "Paperclipse 5", "Paperclips description 5"));
-	}
-	
-	public static List<Product> findAll() {
-		return new ArrayList<Product>(products);
-	}
-	
-	public static Product findByEan(String ean) {
-		for (Product candidate : products) {
-			if (candidate.ean.equals(ean)) {
-				return candidate;
-			}
-		}
-		return null;
-	}
-	
-	public static List<Product> findByName(String term) {
-		final List<Product> results = new ArrayList<Product>();
-		for (Product candidate : products) {
-			if (candidate.name.toLowerCase().contains(term.toLowerCase())) {
-				results.add(candidate);
-			}
-		}
-		
-		return results;
-	}
-	
-	public static boolean remove(Product product) {
-		return products.remove(product);
-	}
-	
-	public void save() {
-		products.remove(findByEan(this.ean));
-		products.add(this);
-	}
+import java.util.*;
+import utils.*;
 
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
 
-	@Override
-	public String javascriptUnbind() {
-		return this.ean;
-	}
+import java.lang.annotation.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
-	@Override
-	public String unbind(String key) {
-		return this.ean;
-	}
+import javax.validation.*;
+import javax.validation.metadata.*;
 
-	@Override
-	public Product bind(String key, String value) {
-		return findByEan(value);
-	}
+public class Product implements PathBindable<Product>,
+    QueryStringBindable<Product> {
 
+  private static List<Product> products;
 
+  @Target({FIELD})
+  @Retention(RUNTIME)
+  @Constraint(validatedBy = EanValidator.class)
+  @play.data.Form.Display(name="constraint.ean", attributes={"value"})
+  public static @interface EAN {
+      String message() default EanValidator.message;
+      Class<?>[] groups() default {};
+      Class<? extends Payload>[] payload() default {};
+  }
 
+  public static class EanValidator extends Constraints.Validator<String> implements ConstraintValidator<EAN, String> {
+    final static public String message = "error.invalid.ean";
+        
+    public EanValidator() {}
+
+    @Override
+    public void initialize(EAN constraintAnnotation) {}
+    
+    @Override
+    public boolean isValid(String value) {
+      String pattern = "^[0-9]{13}$";
+      return value != null && value.matches(pattern);
+    }
+
+    @Override
+    public F.Tuple<String, Object[]> getErrorMessageKey() {
+      return new F.Tuple<String, Object[]>(message,
+          new Object[]{});
+    }
+  }
+
+  static {
+    products = new ArrayList<Product>();
+    products.add(new Product("1111111111111", "Paperclips 1",
+        "Paperclips description 1"));
+    products.add(new Product("2222222222222", "Paperclips 2",
+        "Paperclips description "));
+    products.add(new Product("3333333333333", "Paperclips 3",
+        "Paperclips description 3"));
+    products.add(new Product("4444444444444", "Paperclips 4",
+        "Paperclips description 4"));
+    products.add(new Product("5555555555555", "Paperclips 5",
+        "Paperclips description 5"));
+  }
+
+  @Constraints.Required
+  @EAN
+  public String ean;
+  @Constraints.Required
+  public String name;
+  public String description;
+  public byte[] picture;
+  public Date date = new Date();
+  @DateFormat("yyyy-MM-dd")
+  public Date peremptionDate = new Date();
+
+  public List<Tag> tags = new LinkedList<Tag>();
+
+  public Product() {
+    // Left empty
+  }
+
+  public Product(String ean, String name, String description) {
+    this.ean = ean;
+    this.name = name;
+    this.description = description;
+  }
+
+  public String toString() {
+    return String.format("%s - %s", ean, name);
+  }
+
+  public static List<Product> findAll() {
+    return new ArrayList<Product>(products);
+  }
+
+  public static Product findByEan(String ean) {
+    for (Product candidate : products) {
+      if (candidate.ean.equals(ean)) {
+        return candidate;
+      }
+    }
+    return null;
+  }
+
+  public static List<Product> findByName(String term) {
+    final List<Product> results = new ArrayList<Product>();
+    for (Product candidate : products) {
+      if (candidate.name.toLowerCase().contains(term.toLowerCase())) {
+        results.add(candidate);
+      }
+    }
+
+    return results;
+  }
+
+  public static boolean remove(Product product) {
+    return products.remove(product);
+  }
+
+  public void save() {
+    products.remove(findByEan(this.ean));
+    products.add(this);
+  }
+
+  @Override
+  public Product bind(String key, String value) {
+    return findByEan(value);
+  }
+
+  @Override
+  public F.Option<Product> bind(String key, Map<String, String[]> data) {
+    return F.Option.Some(findByEan(data.get("ean")[0]));
+  }
+
+  @Override
+  public String unbind(String s) {
+    return this.ean;
+  }
+
+  @Override
+  public String javascriptUnbind() {
+    return this.ean;
+  }
 }
